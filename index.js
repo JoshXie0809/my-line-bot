08c345e113af1f903b6fd09ef7cb0540a3fe7410
Josh <61863381+JoshXie0809@users.noreply.github.com>
2025-04-24 19:40:09 +0800

    Initial Commit: Line Bot wit Docker

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const { MessagingApiClient } = require('@line/bot-sdk/messaging-api');
+const { middleware } = require('@line/bot-sdk');
+
+const app = express();
+
+const config = {
+    channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.Line_CHANNEL_SECRET,
+};
+
+app.post('/webhook', middleware(config), (req, res) => {
+    Promise.all(req.body.envents,map(handleEvent))
+        .then( result => res.json(result) )
+        .catch(err => {
+            console.error(err);
+            res.status(500).end();
+        });
+
+});
+
+const client = new MessagingApiClient(config);
+
+function handleEvent(event) {
+    if(event.type !== 'message' || event.message.type !== 'text') {
+        return Promise.resolve(null);
+    }
+
+    return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: `你說了: ${event.message.text}`
+    });
+}
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
